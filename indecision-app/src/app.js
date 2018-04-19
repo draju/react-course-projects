25e182f32722284686c2de9506299f6d20748406
draju <4461424+draju@users.noreply.github.com>
2018-04-19 16:21:27 -0400

    add form and handler

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -28,23 +28,29 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+	handlePick(){
+		alert("handlePick");
+	}
 	render(){
 		return (
 			<div>
-				<button>What should I do?</button>
+				<button onClick={this.handlePick}>What should I do?</button>
 			</div>
 		);
 	}
 }
 
 class Options extends React.Component {
+	handleRemoveAll(){
+		alert('handleRemoveAll');
+	}
 	render(){
 		return (
 			<div>
 			{
 				this.props.options.map((option) => <Option key={option} optionText={option}/>)
 			}
-			<Option />
+			<button onClick={this.handleRemoveAll}>Remove All</button>
 			</div>
 		);
 	}
@@ -61,10 +67,20 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+	handleAddOption(e){
+		e.preventDefault();
+		const option = e.target.elements.option.value.trim(); 
+		if(option){
+		  alert(option);
+		}
+	}
 	render(){
 		return (
 			<div>
-			<p>AddOption component goes here</p>
+				<form onSubmit={this.handleAddOption}>
+				  <input type="text" name="option" />
+				  <button>Add Option</button>
+				</form>
 			</div>
 		);
 	}
