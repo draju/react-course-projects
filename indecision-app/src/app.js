aad3172d25516123a0aac5b1c1010dd90cc3c123
draju <4461424+draju@users.noreply.github.com>
2018-04-19 13:07:23 -0400

    Nesting Components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+	render(){
+		return (
+		  <div>
+			<Header />
+			<Action />
+			<Options />
+			<AddOption />
+		  </div>
+		);		
+	}
+}
+
 class Header extends React.Component {
 
 	render(){
@@ -25,6 +38,17 @@ class Options extends React.Component {
 		return (
 			<div>
 			<p>Options component goes here</p>
+			<Option />
+			</div>
+		);
+	}
+}
+
+class Option extends React.Component {
+	render(){
+		return (
+			<div>
+			<p>Option component goes here </p>
 			</div>
 		);
 	}
@@ -49,4 +73,4 @@ const jsx = (
 	</div>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
