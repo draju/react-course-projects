aaebfdd80607d8260bbb769c8ee06fb3de2bdf94
draju <4461424+draju@users.noreply.github.com>
2018-04-21 11:40:26 -0400

    pass event handlers to child components using props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,41 @@
 class IndecisionApp extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+		this.handlePick = this.handlePick.bind(this);
+		this.state = {
+			options: ['Thing one', 'Thing two','Thing three']
+		}
+	}
+	//Pass event handlers to child components so they can manipulate data of parent
+	handleDeleteOptions(){
+		this.setState(() => {
+			return {
+				options: []
+			};
+		});
+	}
+	handlePick(){
+		if(this.state.options.length > 0){
+			const pickedOption = Math.floor(Math.random()*this.state.options.length);
+			alert(this.state.options[pickedOption]);
+		}
+	}
 	render(){
 		const title = "Indecision";
 		const subtitle = "Put your life in the hands of a computer.";
-		const options = ['Thing one', 'Thing two','Thing four'];
 
 		return (
 		  <div>
 			<Header title={title} subtitle={subtitle}/>
-			<Action />
-			<Options options={options} />
+			<Action 
+				hasOptions={this.state.options.length > 0}
+				handlePick={this.handlePick} 
+			/>
+			<Options 
+				options={this.state.options} 
+				handleDeleteOptions={this.handleDeleteOptions}
+			/>
 			<AddOption />
 		  </div>
 		);		
@@ -28,29 +55,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-	handlePick(){
-		alert("handlePick");
-	}
 	render(){
 		return (
 			<div>
-				<button onClick={this.handlePick}>What should I do?</button>
+				<button 
+					onClick={this.props.handlePick}
+					disabled={!this.props.hasOptions}
+				>
+				What should I do?
+				</button>
 			</div>
 		);
 	}
 }
 
 class Options extends React.Component {
-	handleRemoveAll(){
-		alert('handleRemoveAll');
-	}
 	render(){
 		return (
 			<div>
 			{
 				this.props.options.map((option) => <Option key={option} optionText={option}/>)
 			}
-			<button onClick={this.handleRemoveAll}>Remove All</button>
+			<button onClick={this.props.handleDeleteOptions}>Remove All</button>
 			</div>
 		);
 	}
