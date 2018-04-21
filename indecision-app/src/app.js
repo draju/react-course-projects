2700ec847471f1f4a4e2375a8e5aa6f1fdcfc5d9
draju <4461424+draju@users.noreply.github.com>
2018-04-21 14:12:46 -0400

    pass data from child to parent with error handling

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
 		super(props);
 		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
 		this.handlePick = this.handlePick.bind(this);
+		this.handleAddOption = this.handleAddOption.bind(this);
 		this.state = {
-			options: ['Thing one', 'Thing two','Thing three']
+			options: []
 		}
 	}
 	//Pass event handlers to child components so they can manipulate data of parent
@@ -21,6 +22,19 @@ class IndecisionApp extends React.Component {
 			alert(this.state.options[pickedOption]);
 		}
 	}
+	handleAddOption(option){
+		if(!option){ //check for empty string
+			return 'Must pass non-empty string';
+		} else if (this.state.options.indexOf(option) > -1){
+			return 'Duplicate options not allowed';
+		}
+
+		this.setState((prevState) => {
+			return {
+				options: prevState.options.concat(option)
+			}
+		});
+	}
 	render(){
 		const title = "Indecision";
 		const subtitle = "Put your life in the hands of a computer.";
@@ -36,7 +50,9 @@ class IndecisionApp extends React.Component {
 				options={this.state.options} 
 				handleDeleteOptions={this.handleDeleteOptions}
 			/>
-			<AddOption />
+			<AddOption 
+				handleAddOption = {this.handleAddOption}
+			/>
 		  </div>
 		);		
 	}
@@ -93,16 +109,32 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleAddOption = this.handleAddOption.bind(this);
+		this.state = {
+			error: undefined
+		}
+	}
 	handleAddOption(e){
 		e.preventDefault();
 		const option = e.target.elements.option.value.trim(); 
-		if(option){
-		  alert(option);
+		const error = this.props.handleAddOption(option);
+		
+		if(error){
+		  this.setState(() => {
+				return {   //can use the ES6 shorthand return { error }; since two objects have same name
+					error: error
+				};
+			});
 		}
 	}
 	render(){
 		return (
 			<div>
+				{ 
+					this.state.error && <p>{this.state.error}</p>
+				}
 				<form onSubmit={this.handleAddOption}>
 				  <input type="text" name="option" />
 				  <button>Add Option</button>
